1b8426ba1524aec9f6b3118bfde593e2abf2edb6
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2025-01-13 12:21:34 +0900

    fix: [한재혁] import 문제 해결 [IHFS-2]

--- generate_questions/controller/request_form/generate_questions_request_form.py
+++ generate_questions/controller/request_form/generate_questions_request_form.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 
+from generate_questions.service.request.generate_questions_request import GenerateQuestionsRequest
+
 
 class GenerateQuestionsRequestForm(BaseModel):
     userToken: str
 
-    def toGenerateQuestionsRequest(self) -> GenerateQuestionRequest:
+    def toGenerateQuestionsRequest(self) -> GenerateQuestionsRequest:
         return GenerateQuestionsRequest(userToken=self.userToken)
